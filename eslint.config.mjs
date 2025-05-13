cd9339ca73cabe8d3b9f3e8e0dad259fd848621a
Rudy Nurafif <rudynurafif@gmail.com>
2025-05-14 00:48:01 +0700

    REST API with Neon PostgreSQL

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -30,6 +30,11 @@ export default tseslint.config(
       '@typescript-eslint/no-floating-promises': 'warn',
       '@typescript-eslint/no-unsafe-argument': 'warn',
       '@typescript-eslint/no-unsafe-call': 'off',
+      // Note: you must disable the base rule as it can report incorrect errors
+      'no-unused-vars': 'off',
+      '@typescript-eslint/no-unused-vars': 'off',
+      'require-await': 'off',
+      '@typescript-eslint/require-await': 'off',
     },
   },
 );
